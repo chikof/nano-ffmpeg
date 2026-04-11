856b0ae21b22ebe117ceaa38ee612044557fdbce
Aakash Pahuja <pahujaaakash5@gmail.com>
2026-04-11 19:10:43 +0100

    build: add GoReleaser config and Homebrew formula template
    Cross-platform builds for linux/darwin/windows (amd64/arm64).
    Homebrew tap auto-publishes to dgr8akki/homebrew-tap on release.
    Template formula included as reference.

---
+++ homebrew/nano-ffmpeg.rb
@@ -0,0 +1,40 @@
+# This is a template -- GoReleaser auto-generates the real formula in the homebrew-tap repo.
+# See .goreleaser.yaml for the brew section that drives generation.
+# The actual formula will be published to https://github.com/dgr8akki/homebrew-tap
+
+class NanoFfmpeg < Formula
+  desc "A beautiful terminal UI for ffmpeg"
+  homepage "https://github.com/dgr8akki/nano-ffmpeg"
+  version "0.0.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/dgr8akki/nano-ffmpeg/releases/download/v#{version}/nano-ffmpeg_#{version}_darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/dgr8akki/nano-ffmpeg/releases/download/v#{version}/nano-ffmpeg_#{version}_darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/dgr8akki/nano-ffmpeg/releases/download/v#{version}/nano-ffmpeg_#{version}_linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/dgr8akki/nano-ffmpeg/releases/download/v#{version}/nano-ffmpeg_#{version}_linux_amd64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  depends_on "ffmpeg"
+
+  def install
+    bin.install "nano-ffmpeg"
+  end
+
+  test do
+    system "#{bin}/nano-ffmpeg", "--version"
+  end
+end
